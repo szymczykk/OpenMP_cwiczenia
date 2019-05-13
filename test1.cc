015423de691e31553162bccdff5d6b4b67e1ed43
szymczykk <szymczyk.k@gmail.com>
2019-05-13 08:36:56 +0200

    commit

---
+++ test1.cc
@@ -0,0 +1,24 @@
+#include <omp.h>
+#include <iostream>
+using namespace std;
+int main(){
+int n_threads, thread_id;
+//Utwórz blok rownolegly 4 watkow przekazujac kazdemu z nich kopie
+//zmiennej n_threads oraz thread_id
+//
+//Do zmiennej thread_id przypisz numer biezacego watku
+//i ponizsza linia wypisze ten numer
+#pragma omp parallel num_threads(4) private (n_threads, thread_id)
+thread_id = omp_get_thread_num();
+cout<< "Watek nr= " << thread_id << endl;
+
+//Niech watek glowny wykona ponizszy kawalek kodu
+if(thread_id == 0)
+{
+//Do n_threads przypisz liczbe watkow
+n_threads = omp_get_num_threads();
+cout << "Liczba watkow = " << n_threads << endl;
+}
+
+// ### UWAGA powyzej w miejsca ... nalezy cos wpisac
+}
